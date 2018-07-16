1b5b224b042ccc3586e6131015cdf753aace0871
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-07-15 20:49:46 -0700

    Setup graphiql interface and support menu_items

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -9,11 +9,20 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
+  alias PlateSlate.{Menu, Repo}
+
   query do
-    # <<Ignore this for now>>
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ ->
+        {:ok, Repo.all(Menu.Item)}
+      end
+    end
   end
 
   object :menu_item do
-    # <<We'll add fields soon>>
+    field :id, :id
+    field :name, :string
+    field :description, :string
   end
 end
