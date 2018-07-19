78d5a8953a55a41f6ada5cff9b8c0b706cab4185
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-07-18 17:43:41 -0700

    Use input_object and input_field to split schema fields

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,21 +1,17 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
-  alias PlateSlate.{Menu, Repo}
   alias PlateSlateWeb.Resolvers
-
-  import Ecto.Query
-
+  import_types __MODULE__.MenuTypes
 
   enum :sort_order do
     value :asc
     value :desc
   end
 
-
   scalar :date do
     parse fn input ->
-      with %Absinthe.Blueprint.Input.String{value: value} <- input,  
+      with %Absinthe.Blueprint.Input.String{value: value} <- input,
         {:ok, date} <- Date.from_iso8601(value) do
           {:ok, date}
       else
@@ -28,40 +24,9 @@ defmodule PlateSlateWeb.Schema do
     end
   end
 
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
-    field :name, :string
-
-    @desc "Matching a category name"
-    field :category, (:string)
-
-    @desc "Matching a tag"
-    field :tag, :string
-
-    @desc "Priced above a value"
-    field :priced_above, :float
-
-    @desc "Priced below a value"
-    field :priced_below, :float
-
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-
-    @desc "Added to the menu after this date"
-    field :added_after, :date
-  end
-
 
   query do
-    @desc "The list of available items on the menu"
-    field :menu_items, list_of(:menu_item) do
-
-      arg :filter, (:menu_item_filter)
-      arg :order, type: :sort_order, default_value: :asc
-
-      resolve &Resolvers.Menu.menu_items/3
-    end
+    import_fields :menu_queries
 
     @desc "The list of categories"
     field :categories, list_of(:category) do
@@ -73,13 +38,6 @@ defmodule PlateSlateWeb.Schema do
     end
   end
 
-
-  object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
-    field :added_on, :date
-  end
 
   object :category do
     field :id, :id
