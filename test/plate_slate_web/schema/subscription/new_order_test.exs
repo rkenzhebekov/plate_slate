957e91af113588de1075417dca829261e2a9db41
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-10-05 18:17:43 -0700

    Authorizatoin Middleware

--- test/plate_slate_web/schema/subscription/new_order_test.exs
+++ test/plate_slate_web/schema/subscription/new_order_test.exs
@@ -15,7 +15,23 @@ defmodule PlateSlateWeb.Schema.Subscription.NewOrderTest do
   }
   """
 
+  @login """
+  mutation ($email: String!, $role: Role!) {
+    login(role: $role, password: "super-secret", email: $email) {
+      token
+    }
+  }
+  """
+
   test "new orders can be subscribed to", %{socket: socket} do
+    # login
+    user = Factory.create_user("employee")
+    ref = push_doc socket, @login, variables: %{
+      "email" => user.email,
+      "role" => "EMPLOYEE"
+    }
+    assert_reply ref, :ok, %{data: %{"login" => %{"token" => _}}}, 1_000
+
     # setup a subscription
     ref = push_doc socket, @subscription
     assert_reply ref, :ok, %{subscriptionId: subscription_id}
